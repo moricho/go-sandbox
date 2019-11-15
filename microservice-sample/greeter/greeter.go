d751750302f0f608a866936e29e75664638e6568
moricho <ikeda.morito@gmail.com>
2019-11-15 16:26:55 +0900

    sample grpc server

--- microservice-sample/greeter/greeter.go
+++ microservice-sample/greeter/greeter.go
@@ -1,1 +1,21 @@
 package greeter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	pb "github.com/moricho/go-sandbox/microservice-sample/greeter/proto"
+)
+
+type GreeterService struct{}
+
+func (gs *GreeterService) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	if req.Name == "" {
+		return nil, errors.New("error: empty input.")
+	}
+	msg := fmt.Sprintf("Hello, %s!", req.Name)
+	return &pb.HelloReply{
+		Message: msg,
+	}, nil
+}
--- microservice-sample/main.go
+++ microservice-sample/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/moricho/go-sandbox/microservice-sample/greeter"
+	pb "github.com/moricho/go-sandbox/microservice-sample/greeter/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	server := grpc.NewServer()
+	greeterService := &greeter.GreeterService{}
+
+	pb.RegisterGreeterServer(server, greeterService)
+	server.Serve(lis)
+}
