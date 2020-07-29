28a967f0f24bb8dcc421eae4b9bce3b2ee27e17c
moricho <ikeda.morito@gmail.com>
2020-07-29 14:41:59 +0900

    initialize casher service

---
+++ casher/domain.go
@@ -0,0 +1,44 @@
+package main
+
+import "time"
+
+type Item struct {
+	ID        int
+	Name      string
+	Price     int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type PurchaseHistory struct {
+	ID        int
+	StoreID   int
+	StaffID   int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Staff struct {
+	ID        int
+	Name      string
+	StoreID   int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Store struct {
+	ID         int
+	Branch     string
+	Address    string
+	CreatedAt  time.Time
+	UpdateddAt time.Time
+}
+
+type Transaction struct {
+	ID         int
+	PurchaseID int
+	ItemID     int
+	Amount     int
+	CreatedAt  time.Time
+	UpdateddAt time.Time
+}
---
+++ casher/go.mod
@@ -0,0 +1,3 @@
+module github.com/moricho/casher
+
+go 1.14
